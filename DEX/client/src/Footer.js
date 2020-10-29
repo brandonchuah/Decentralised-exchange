cdd9929015cf0bcedb21aea82f88f1eade968be0
brandonchuah <73574416+brandonchuah@users.noreply.github.com>
2020-10-29 12:53:52 +0800

    Add files via upload

---
+++ DEX/client/src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p className="text-center">
+  Back to  <a href="http://localhost:8000/" style={{color: 'white'}}> main page</a>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
